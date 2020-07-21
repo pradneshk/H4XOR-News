41dc494e1bffca31f021cfe8a9a3aedf348c88c6
Pradnesh Kore <pkore23@gmail.com>
2020-07-21 18:48:17 +0530

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Pradnesh Kore on 21/07/20.
+//  Copyright © 2020 Pradnesh Kore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
